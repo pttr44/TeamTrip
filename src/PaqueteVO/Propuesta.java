2e1142c025ec6ef496efa7e18b00700d4d7754c2
Artyx3147 <agustinscandolo20@gmail.com>
2026-05-20 12:08:50 +0200

    Métodos usuario/propuesta control

--- src/PaqueteVO/Propuesta.java
+++ src/PaqueteVO/Propuesta.java
@@ -4,10 +4,14 @@ public class Propuesta {
     
     private int id_Propuesta;
     private String propuesta;
+    private String dni;
+    private String estado;
 
-    public Propuesta(int id_Propuesta, String propuesta) {
+    public Propuesta(int id_Propuesta, String propuesta, String dni, String estado) {
         this.id_Propuesta = id_Propuesta;
         this.propuesta = propuesta;
+        this.dni = dni;
+        this.estado = estado;
     }
 
     public int getId_Propuesta() {
@@ -26,8 +30,24 @@ public class Propuesta {
         this.propuesta = propuesta;
     }
 
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
     @Override
     public String toString() {
-        return "Propuesta [id_Propuesta=" + id_Propuesta + ", propuesta=" + propuesta + "]";
+        return "Propuesta [id_Propuesta=" + id_Propuesta + ", propuesta=" + propuesta + ", dni=" + dni + ", estado=" + estado + "]";
     }
 }
